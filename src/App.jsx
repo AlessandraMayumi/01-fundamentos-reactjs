9a5454e8adf794b309704d48dd232a9769ec0655
AlessandraMayumi <alessandra_mms@hotmail.com>
2023-03-17 09:13:47 -0300

    Initialize Vite Javascript

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Post } from "./Post"
+import { Header } from "./components/Header"
+import './styles.css'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Post author="batata quente quente" />
+    </>
+  )
+}
+
+export default App
