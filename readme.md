11611eacd4474c1f77c6988f8c09304b9c514b98
AlessandraMayumi <alessandra_mms@hotmail.com>
2023-03-25 11:02:30 -0300

    Add CSS for sidebar component

---
+++ readme.md
@@ -0,0 +1,3 @@
+site to find free images https://unsplash.com/
+
+icons https://phosphoricons.com/
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { Post } from "./Post"
 import { Header } from "./components/Header"
+import styles from './App.module.css'
 import './global.css'
+import { Sidebar } from "./components/Sidebar"
 
 function App() {
 
   return (
     <>
       <Header />
-      <Post author="batata quente quente" />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+        <Post author="batata quente quente" />
+        </main>
+      </div>
     </>
   )
 }
